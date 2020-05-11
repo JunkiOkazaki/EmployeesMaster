dbd600e1b8d572dad6bfbeb64381cf48efa162e5
JunkiOkazaki <j_okazaki@mail.jokazaki.net>
2020-05-11 18:28:31 +0900

    202005111828

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
     <script>
         $( function() {
             $( "#datepicker" ).datepicker({
-            dateFormat: 'yymmdd',
+            dateFormat: 'yy-mm-dd',
             yearSuffix: '年',
             showMonthAfterYear: true,
             monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
@@ -46,9 +46,6 @@
 
 <?php
 session_start();
-//if (isset($_POST['name'])) {
-//    $_SESSION['name'] = $_POST['name'];//入力された値をセッションに代入する
-//}
 
 foreach ($_POST as $key => $value){
     $_SESSION[$key] = $value;
@@ -71,14 +68,11 @@ foreach ($_POST as $key => $value){
 
 <form method="post" action="index.php">
     <div  class="cp_iptxt"><input class="ef" type="text" name="employee_id" size="30" placeholder=""><label>従業員ID</label><span class="focus_line"></span></div>
-    <!-- <div>従業員コード <input type="text" name="employee_code" size="50"></div> -->
     <div  class="cp_iptxt"><input class="ef" type="text" name="employee_name" size="30" placeholder=""><label>氏　　名</label><span class="focus_line"></span></div>
     <div  class="cp_iptxt"><input class="ef" type="text" name="department_id" size="30" placeholder=""><label>部　署ID</label><span class="focus_line"></span></div>
-    <!-- <div>削除フラグ <input type="text" name="delete_flag" size="50"></div> -->
     <div  class="cp_iptxt"><input class="ef" id="datepicker" type="text" name="created_at" size="30" placeholder="" ><label>登録日時</label><span class="focus_line"></span></div>
-    <!-- <div  class="cp_iptxt"><input class="ef" id="datepicker" type="text" name="updated_at" size="30" placeholder="" ><label>更新日時</label><span class="focus_line"></span></div> -->
     <div><input type="submit" name="filter" value="フィルタ" class="button"></div>
-<br/>    
+<br/>
 
 <?php
 
@@ -94,61 +88,36 @@ try{
 <?php
 try{
     $employee_id = $_SESSION['employee_id'];
-    //echo $employee_id;
     $employee_code = $_SESSION['employee_code'];
     $employee_name = $_SESSION['employee_name'];
-    //echo $employee_name;
     $department_id = $_SESSION['department_id'];
-    //echo $department_id;
-    //$delete_flag = $_SESSION['delete_flag'];
-    //$delete_flag = 1;
+    $delete_flag = $_SESSION['delete_flag'];
     $created_at = $_SESSION['created_at'];
-    //echo $created_at;
-    //echo "<br />";
-    //echo gettype($created_at);
-    //$updated_at = $_SESSION['updated_at'];
-    //$sql = "SELECT * FROM company.employees WHERE employee_id=? OR employee_code=? OR employee_name LIKE ? OR department_id=? OR delete_flag=? OR created_at=? OR updated_at=?";
+    $updated_at = $_SESSION['updated_at'];
     
-        
-//$sql = "SELECT * FROM company.employees WHERE employee_id=?";
-   
-    if(is_numeric($employee_id)){
+     
+    if(!empty($employee_id)){
     $sql = "SELECT * FROM company.employees WHERE employee_id=:employee_id";
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':employee_id', $employee_id, PDO::PARAM_INT);
-    $stmt->execute();
-    }elseif(is_string($employee_name)){
+    }elseif(!empty($employee_name)){
     $sql = "SELECT * FROM company.employees WHERE employee_name LIKE :employee_name";
     $stmt = $pdo->prepare($sql);
     $stmt->bindValue(':employee_name', '%'.$employee_name.'%', PDO::PARAM_STR);
-    $stmt->execute();
-    }elseif(is_numeric($department_id)){
+    }elseif(!empty($department_id)){
     $sql = "SELECT * FROM company.employees WHERE department_id=:department_id";
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':department_id', $department_id, PDO::PARAM_INT);
-    $stmt->execute();
-    }elseif(!is_null($created_at)){
+    }elseif(!empty($created_at)){
     $sql = "SELECT * FROM company.employees WHERE created_at LIKE :created_at";
     $stmt = $pdo->prepare($sql);
-    $stmt->bindValue(':created_at', '%'.$created_at.'%', PDO::PARAM_STR);
-    $stmt->execute();  
+    $stmt->bindParam(':created_at', $created_at, PDO::PARAM_STR);
     }else{
     $sql = "SELECT * FROM company.employees";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute();
     }
-    
-    //$sql = "SELECT * FROM company.employees WHERE employee_name LIKE ?";
-    //$stmt = $pdo->prepare($sql);
-    //$stmt->bindParam(1, $employee_id, PDO::PARAM_INT);
-    //$stmt->bindParam(2, $employee_code, PDO::PARAM_INT);
-    //$stmt->bindValue(1, '%'.$employee_name.'%', PDO::PARAM_STR);
-    //$stmt->bindParam(4, $department_id, PDO::PARAM_INT);
-    //$stmt->bindParam(5, $delete_flag, PDO::PARAM_INT);
-    //$stmt->bindParam(6, $created_at, PDO::PARAM_STR);
-    //$stmt->bindParam(7, $updated_at, PDO::PARAM_STR);
-
-    
+    $stmt->execute();
+        
 }catch(PDOException $Exception){
     die('接続エラー：' .$Exception->getMessage());
 }
