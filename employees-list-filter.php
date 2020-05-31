d05e61ca2b047f91383e755c1ef2409c4d01c418
JunkiOkazaki <j_okazaki@mail.jokazaki.net>
2020-06-01 06:48:42 +0900

    セキュリティ対策

--- employees-list-filter.php
+++ employees-list-filter.php
@@ -28,9 +28,9 @@
 
         <!-- jQuery Datepicker -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <link rel="stylesheet" href="./jquery-ui-1.12.1/jquery-ui.css">
+        <script src="jquery-1.12.4.js"></script>
+        <script src="jquery-ui.js"></script>
 
         <script>
             $(function () {
